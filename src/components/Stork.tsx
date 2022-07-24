212825d5ba385fbf6ff887f3c7e276ea94ba6ea2
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2022-07-24 22:48:02 +0900

    Fix to register a stork index

--- src/components/Stork.tsx
+++ src/components/Stork.tsx
@@ -16,45 +16,69 @@
 
 import Head from 'next/head';
 import Script from 'next/script';
-import {FunctionComponent, useEffect} from 'react';
+import {FunctionComponent} from 'react';
 import {isStorkEnabled} from '@/model/configuration';
 
-// https://stork-search.net/docs/js-ref/
-// https://github.com/jameslittle230/stork/tree/master/js
-type StorkOptions = {
-  showProgress?: boolean;
-  printIndexInfo?: boolean;
-  showScores?: boolean;
-  minimumQueryLength?: number;
-  onQueryUpdate?: (query: string, result: any) => void;
-  onResultSelected?: (query: string, result: any) => void;
-};
+/**
+ * @see https://github.com/jameslittle230/stork/blob/v1.5.0/js/config.ts
+ */
+interface StorkOptions {
+  showProgress: boolean;
+  printIndexInfo: boolean;
+  showScores: boolean;
+  showCloseButton: boolean;
+  minimumQueryLength: number;
+  forceOverwrite: boolean;
+  resultNoun: { singular: string; plural: string };
+  onQueryUpdate: (query: string, results: StorkResult[]) => unknown;
+  onResultSelected: (query: string, result: StorkResult) => unknown;
+  onResultsHidden: () => unknown;
+  onInputCleared: () => unknown;
+  transformResultUrl: (url: string) => string;
+}
 
+/**
+ * @see https://github.com/jameslittle230/stork/blob/v1.5.0/js/searchData.ts
+ */
+interface StorkResult {
+  entry: {
+    fields: Record<string, unknown>;
+    title: string;
+    url: string;
+  };
+  expects: Array<{
+    fields: Record<string, unknown>;
+    internal_annotations?: Array<Record<string, unknown>>;
+    highlight_ranges?: Array<HighlightRange>;
+    score: number;
+    text: string;
+  }>;
+  score: number;
+  title_highlight_ranges?: Array<HighlightRange>;
+}
+
+/**
+ * @see https://github.com/jameslittle230/stork/blob/v1.5.0/js/searchData.ts
+ */
+interface HighlightRange {
+  beginning: number;
+  end: number;
+}
+
+/**
+ * @see https://github.com/jameslittle230/stork/blob/v1.5.0/js/main.ts
+ */
 declare const stork: {
-  register(name: string, url: string, options?: StorkOptions): void;
-} | undefined;
+  initialize(wasmOverrideUrl?: string): Promise<void>;
+  downloadIndex(name: string, url: string, config?: Partial<StorkOptions>): Promise<void>;
+  attach(name: string): void;
+  register(name: string, url: string, config?: Partial<StorkOptions>): Promise<void>;
+  search(name: string, query: string): StorkResult[];
+};
 
 const storkName = 'blog';
 
 const Stork: FunctionComponent<unknown> = () => {
-  useEffect(() => {
-    if (!isStorkEnabled) {
-      return;
-    }
-
-    const waitStork = async () => {
-      const timer = (millis: number) => new Promise(resolve => window.setTimeout(resolve, millis));
-      while (!stork) {
-        await timer(100);
-      }
-    };
-
-    (async () => {
-      await waitStork();
-      stork!.register(storkName, '/blog.st', {minimumQueryLength: 2});
-    })();
-  }, []);
-
   return <>
     <Head>
       {/* moved to _document.tsx's <link rel="stylesheet" /> for fast reload. */}
@@ -63,7 +87,16 @@ const Stork: FunctionComponent<unknown> = () => {
       <input className="stork-input" data-stork={storkName} placeholder="Search..."/>
       <div className="stork-output" data-stork={`${storkName}-output`}/>
     </div>
-    <Script src="https://files.stork-search.net/releases/v1.5.0/stork.js"/>
+    <Script
+      src="https://files.stork-search.net/releases/v1.5.0/stork.js"
+      strategy="lazyOnload"
+      onLoad={async () => {
+        if (!isStorkEnabled) {
+          return;
+        }
+        await stork.register(storkName, '/blog.st', {minimumQueryLength: 2});
+      }}
+    />
   </>;
 };
 
